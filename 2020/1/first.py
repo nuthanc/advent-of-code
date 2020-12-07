ab15a26343623145ee96833949665f8899a412db
Nuthan Chandra <nuthanc@juniper.net>
2020-12-07 22:07:01 +0530

    Part 1 answer,don't know whether it's efficient

--- 2020/1/first.py
+++ 2020/1/first.py
@@ -1,3 +1,27 @@
-# Report Repair
+# Report Repair: https://adventofcode.com/2020/day/1
 
 # Find the two entries that sum to 2020
+with open('first.txt') as f:
+    expenses_string = f.read().splitlines()
+
+expenses = list(map(int, expenses_string))
+expenses.sort()
+print(expenses)
+
+start_index = 0
+end_index = len(expenses) - 1
+sum = expenses[start_index] + expenses[end_index]
+# import pdb;pdb.set_trace()
+while sum != 2020:
+    if sum > 2020:
+        end_index -= 1
+    elif sum < 2020:
+        start_index +=1
+    else:
+        break
+    sum = expenses[start_index] + expenses[end_index]
+    # print(expenses[start_index], expenses[end_index], sum)
+
+
+print(expenses[start_index], expenses[end_index])
+print(expenses[start_index]*expenses[end_index])
