406f025c24bda8b673b62012446824e929b69c95
Nuthan Chandra <nuthanc@juniper.net>
2020-12-17 20:04:31 +0530

    Successful day 5 part 1 answer

---
+++ 2020/5/second.py
@@ -0,0 +1,38 @@
+# Day 5: Binary Boarding: https://adventofcode.com/2020/day/5
+
+from os import path
+THIS_DIR = path.dirname(path.realpath(__file__))
+file_path = path.join(THIS_DIR, 'input.txt')
+with open(file_path) as f:
+    passes = f.read().splitlines()
+
+max = 0
+min = 0
+for p in passes:
+    # row_ind = p[0:7]
+    # seat_ind = p[-3:]
+    row_low = 0
+    row_high = 127
+    col_low = 0
+    col_high = 7
+    for ch in p:
+        if ch == 'F':
+            row_high = (row_low + row_high)//2
+        if ch == 'B':
+            row_low = (row_low + row_high)//2 + 1
+        if ch == 'L':
+            col_high = (col_low + col_high)//2
+        if ch == 'R':
+            col_low = (col_low + col_high)//2 + 1
+    row = row_low
+    col = col_low
+    # print(row, col)
+    id = row*8 + col
+    # print(id)
+    if id > max:
+        max = id
+    if id < min:
+        min = id
+
+print("max id:", max)
+print("min id:", min)
