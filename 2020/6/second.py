4f785eb4beac7127e528130342d8902773551a44
Nuthan Chandra <nuthanc@juniper.net>
2020-12-18 17:47:15 +0530

    Successfully solved part 2 after using pdb

--- 2020/6/second.py
+++ 2020/6/second.py
@@ -8,9 +8,24 @@ with open(file_path) as f:
 
 count = 0
 customs = inputs.split("\n\n")
+# print(customs)
 for custom in customs:
-    custom = custom.replace('\n','')
-    # print(custom)
-    count += len(set(custom))
+    # This part is error free
+    if '\n' not in custom:
+        count += len(custom)
+    else:
+        uniq = set()
+        uniq_dict = {}
+        for ele in custom:
+            if ele == '\n':
+                continue
+            if ele in uniq:
+                uniq_dict[ele] +=  1
+                if uniq_dict[ele] == (custom.count('\n') + 1):
+                    count += 1
+            else:
+                uniq.add(ele)
+                uniq_dict[ele] = 1
+        # print(uniq_dict)
 
 print(count)
