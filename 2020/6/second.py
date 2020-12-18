3d549a313e51b7c907b9bdd99f0d2fa9caa7ef71
Nuthan Chandra <nuthanc@juniper.net>
2020-12-18 16:41:09 +0530

    Successful day 6 part 1

---
+++ 2020/6/second.py
@@ -0,0 +1,16 @@
+# Day 6: Custom Customs: https://adventofcode.com/2020/day/6#part2
+
+from os import path
+THIS_DIR = path.dirname(path.realpath(__file__))
+file_path = path.join(THIS_DIR, 'input.txt')
+with open(file_path) as f:
+    inputs = f.read()
+
+count = 0
+customs = inputs.split("\n\n")
+for custom in customs:
+    custom = custom.replace('\n','')
+    # print(custom)
+    count += len(set(custom))
+
+print(count)
