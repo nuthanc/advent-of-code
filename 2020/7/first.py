0e5ce4ecef89f907abfafb1afef1a4b48fcd2e82
Nuthan Chandra <nuthanc@juniper.net>
2020-12-22 23:47:13 +0530

    Initial super parsing

--- 2020/7/first.py
+++ 2020/7/first.py
@@ -7,11 +7,24 @@ with open(file_path) as f:
     bag_list = f.read().splitlines()
 
 max = 0
+bag_dict = {}
+direct_container_bags = []
+req_bag = 'shiny gold'
 for line in bag_list:
     bag_string, content = line.split("contain")
     content_list = re.findall(r'(\d)([\w\s]+)bag.', content)
     contrast, color, _ = bag_string.split("bags")[0].split(" ")
-    bag = f'{contrast}_{color}'
-    print(bag)
-    print(content_list)
+    bag = f'{contrast} {color}'
+    # print(bag)
+    # print(content_list)
+    contained_bags = [bag.strip() for num, bag in content_list]
+    bag_dict[bag] = contained_bags
+    
+    if req_bag in contained_bags:
+        direct_container_bags.append(bag)
+    
 
+for k,v in bag_dict.items():
+    print(f'{k}: {v}')
+
+print(direct_container_bags)
