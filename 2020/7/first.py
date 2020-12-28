1ac3812071c8ad0167c7ffcd13593cdd30aee185
Nuthan Chandra <nuthanc@juniper.net>
2020-12-28 23:31:22 +0530

    Successful day 7 part 1 solution

--- 2020/7/first.py
+++ 2020/7/first.py
@@ -6,12 +6,52 @@ file_path = path.join(THIS_DIR, 'input.txt')
 with open(file_path) as f:
     bag_list = f.read().splitlines()
 
-max = 0
-for line in bag_list:
-    bag_string, content = line.split("contain")
-    content_list = re.findall(r'(\d)([\w\s]+)bag.', content)
-    contrast, color, _ = bag_string.split("bags")[0].split(" ")
-    bag = f'{contrast}_{color}'
-    print(bag)
-    print(content_list)
+bag_dict = {}
+direct_container_bags = []
+req_bag = 'shiny gold'
+parent_bag_dict = {}
 
+for line in bag_list:
+    container_bags, content = line.split("contain")
+    content_list = re.findall(r'(\d)([\w\s]+)bag.', content)
+    contrast, color, _ = container_bags.split("bags")[0].split(" ")
+    container_color = f'{contrast} {color}'
+    # print(container_bags)
+    # print(content_list)
+    contained_bags = [bag.strip() for num, bag in content_list]
+    bag_dict[container_color] = contained_bags
+
+    for bag in contained_bags:
+        if req_bag == bag:
+            direct_container_bags.append(container_color)
+        if parent_bag_dict.get(bag):
+            parent_bag_dict[bag].append(container_color)
+        else:
+            parent_bag_dict[bag] = [container_color]
+
+
+
+# for k, v in bag_dict.items():
+#     print(f'{k}: {v}')
+for k,v in parent_bag_dict.items():
+    print(f'{k}: {v}')
+print(direct_container_bags)
+
+uniq_set = set()
+
+
+def recursively_add_to_set(bag):
+    if bag in uniq_set:
+        return
+    else:
+        uniq_set.add(bag)
+        if parent_bag_dict.get(bag) is not None:
+            for b in parent_bag_dict[bag]:
+                recursively_add_to_set(b)
+
+for bag in direct_container_bags:
+    recursively_add_to_set(bag)
+
+    
+print(uniq_set)
+print(len(uniq_set))
