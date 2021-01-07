696667e6633358e1d0d50c0e5a3cfe697c227915
Nuthan Chandra <nuthanc@juniper.net>
2021-01-07 22:57:59 +0530

    Successful day 7 part 2

--- 2020/7/second.py
+++ 2020/7/second.py
@@ -3,7 +3,7 @@ import re
 from os import path
 
 THIS_DIR = path.dirname(path.realpath(__file__))
-file_path = path.join(THIS_DIR, 'input2.txt')
+file_path = path.join(THIS_DIR, 'input.txt')
 with open(file_path) as f:
     bag_list = f.read().splitlines()
 
@@ -16,28 +16,32 @@ for line in bag_list:
     contained_bags = [(num, bag.strip()) for num, bag in content_list]
     bag_dict[container_color] = contained_bags
 
-for k,v in bag_dict.items():
-    print(k,v)
+# for k,v in bag_dict.items():
+#     print(k,v)
 
 # shiny gold  [('5', 'light black'), ('3', 'mirrored yellow'), ('5', 'muted plum')]
 
 # 1: Recursion
 # Check this for recursion in for loop: https://stackoverflow.com/questions/4795527/how-recursion-works-inside-a-for-loop#:~:text=Just%20because%20the%20function%20happens,functions%20again%2C%20and%20so%20on.&text=For%20recursion%2C%20it's%20helpful%20to,stack%20structure%20in%20your%20mind.
-# My solution doesn't consider the container bags, it only considers the contained bags
+# My earlier solution doesn't consider the container bags, it only considers the contained bags
+
 def find_bags(bags):
     if len(bags) == 0:
         return 1
 
     part_sum = 0
     for num, bag in bags:
-        # import pdb;pdb.set_trace()
         n = int(num)
-        part_sum += n * find_bags(bag_dict[bag])
-        print(bag, n, part_sum)
+        val = find_bags(bag_dict[bag]) # By separating into this, I was able to get the answer
+        if val != 1:
+            part_sum += n + n * val 
+        else:
+            # Initially was part_sum += n *find_bags(bag_dict[bag])
+            part_sum += n * val
+        # print(bag, n, part_sum)
+
     return part_sum # Return must be some value
 
 # print(bag_dict['shiny gold'])
-# print(find_bags(bag_dict['shiny gold']))
-
 
 print(find_bags(bag_dict['shiny gold']))
