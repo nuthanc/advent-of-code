7c56bd1d2af3e9b9081c4a9a6a44592c7a81a78c
Nuthan Chandra <nuthanc@juniper.net>
2021-02-14 16:00:23 +0530

    Solved day 9 part 1

--- 2020/9/first.py
+++ 2020/9/first.py
@@ -5,13 +5,31 @@ file_path = path.join(THIS_DIR, 'input.txt')
 with open(file_path) as f:
     boot_code = f.read().splitlines()
 
-pre_len = 5
+boot_code = list(map(int, boot_code))
+
+pre_len = 25
 pre = set()
+found = False
+pf_index = 0
 
 for i in range(pre_len):
     pre.add(boot_code[i])
 
-print(pre)
+# print(pre)
 
 for i in range(pre_len, len(boot_code)):
-    print(boot_code[i])
+    # print(boot_code[i])
+    for j in range(pf_index, pf_index+pre_len):
+        numToFind = boot_code[i] - boot_code[j]
+        if numToFind in pre and numToFind != boot_code[j]:
+            found = True
+            break
+    if found:
+        pre.remove(boot_code[pf_index])
+        pre.add(boot_code[pf_index+pre_len])
+        pf_index += 1
+        found = False
+    else:
+        print(boot_code[i])
+        break
+
