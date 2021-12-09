0948cb96bf235c5fda18eab49ad496dd064f64a8
Nuthan Chandra <nuthanc@juniper.net>
2021-12-09 12:23:48 +0530

    Add day 4 part 1 sol

---
+++ 2021/4/ans.py
@@ -0,0 +1,54 @@
+from os import path
+THIS_DIR = path.dirname(path.realpath(__file__))
+file_path = path.join(THIS_DIR, 'input.txt')
+with open(file_path) as f:
+    puzzle_input = f.read()
+
+
+def get_winning_board_index(boards_element_indices, boards, numbers_drawn, rows_count, cols_count):
+    for num in numbers_drawn:
+        matrix_len = len(boards[0])
+        for i, board_dict in enumerate(boards_element_indices):
+            indices = board_dict.get(num)
+            if indices:
+                board_dict[num][2] = True
+                rows_count[i][indices[0]] += 1
+                cols_count[i][indices[1]] += 1
+                if rows_count[i][indices[0]] == matrix_len:
+                    return i, num
+
+def first(puzzle_input):
+    puzzle_input = puzzle_input.split('\n\n')
+    numbers_drawn = puzzle_input[0].split(',')
+
+    # print(numbers_drawn)
+    boards = []
+    boards_element_indices = []
+
+    for i in range(1, len(puzzle_input)):
+        board = []
+        board_dict = {}
+        for row, content in enumerate(puzzle_input[i].split('\n')):
+            elements = content.split()
+            for col, ele in enumerate(elements):
+                board_dict[ele] = [row, col,False]
+            board.append(elements)
+        boards.append(board)
+        boards_element_indices.append(board_dict)
+        
+    rows_count = [[0] * len(boards[0]) for _ in range(len(boards))]
+    cols_count = [[0] * len(boards[0]) for _ in range(len(boards))]
+    
+    index, last_num = get_winning_board_index(boards_element_indices, boards,
+                            numbers_drawn, rows_count, cols_count)
+    
+    sum = 0
+    board_dict = boards_element_indices[index]
+    for num, indices in board_dict.items():
+        if not indices[2]:
+            sum += int(num)
+    # print(sum)
+    print(sum * int(last_num))
+
+
+first(puzzle_input)
