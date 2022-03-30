cae9ca6c82bd0a0bc85815019cb78cf2a74367ca
nuthanc <rovanova.nuthan@gmail.com>
2022-03-30 07:07:55 +0530

    Add part 2 answer

--- 2021/14/ans.py
+++ 2021/14/ans.py
@@ -35,9 +35,47 @@ def first(polymer_template, rules, steps):
     print(max(occurence.values()) - min(occurence.values()))
 
 
+def second(polymer_template, rules, steps):
+    pair_count = {}
+    for first, second in zip(polymer_template, polymer_template[1:]):
+        pair = first + second
+        if pair not in pair_count:
+            pair_count[pair] = 0
+        pair_count[pair] += 1
+    for _ in range(steps):
+        new_dict = {}
+        for pair in pair_count:
+            ele = rules[pair]
+            new_pair = pair[0] + ele
+            if new_pair not in new_dict:
+                new_dict[new_pair] = pair_count[pair]
+            else:
+                new_dict[new_pair] += pair_count[pair]
+            new_pair = ele + pair[1]
+            if new_pair not in new_dict:
+                new_dict[new_pair] = pair_count[pair]
+            else:
+                new_dict[new_pair] += pair_count[pair]
+        pair_count = new_dict
+    first_count, second_count = {}, {}
+    for pair in pair_count:
+        first, second = pair
+        if first not in first_count:
+            first_count[first] = pair_count[pair]
+        else:
+            first_count[first] += pair_count[pair]
+        if second not in second_count:
+            second_count[second] = pair_count[pair]
+        else:
+            second_count[second] += pair_count[pair]
+    final_dict = {k: max(first_count.get(k,0), second_count.get(k,0)) for k in (first_count.keys() | second_count.keys())}
+    print(max(final_dict.values()) - min(final_dict.values()))
+
+
 def solution():
     polymer_template, rules = read_file()
-    first(polymer_template, rules, 10)
+    # first(polymer_template, rules, 10)
+    second(polymer_template, rules, 40)
 
 
 solution()
