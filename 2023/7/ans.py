dbbeaad078a21af0cc92fec26494d6c9dda8c2e0
nuthanc <rovanova.nuthan@gmail.com>
2023-12-17 12:19:12 +0530

    Update

--- 2023/7/ans.py
+++ 2023/7/ans.py
@@ -1,17 +1,48 @@
 from os import path
+from functools import cmp_to_key
+from collections import Counter
 
 THIS_DIR = path.dirname(__file__)
 file_path = path.join(THIS_DIR, 'input.txt')
 with open(file_path) as f:
     lines = f.read().splitlines()
 
+hand_bids = []
+for line in lines:
+    hand, bid_str = line.split()
+    hand_bids.append((hand, int(bid_str)))
+
+def comparator(a, b):
+    first_hand, second_hand = a[0], b[0]
+    first_hand_counter = Counter(first_hand)
+    second_hand_counter = Counter(second_hand)
+    first_hand_counter_values = sorted(first_hand_counter.values())
+    second_hand_counter_values = sorted(second_hand_counter.values())
+
+    if first_hand_counter_values[-1] == second_hand_counter_values[-1]:
+        # First Counter values Comparison
+        ind = len(first_hand_counter_values) - 2
+        while ind >= 0 and first_hand_counter_values[ind] == second_hand_counter_values[ind]:
+            ind -= 1
+        if ind >= 0:
+            return first_hand_counter_values[ind] - second_hand_counter_values[ind]
+        # Next Hand Lexicographic Comparison
+        # Need to handle for 'A' > 'Q' and other character cases
+        if first_hand < second_hand:
+            return -1
+        else:
+            return 1
+    else:
+        return first_hand_counter_values[-1] - second_hand_counter_values[-1]
+    
 
 def first():
-    pass
+    sorted_hand_bids = sorted(hand_bids, key=cmp_to_key(comparator))
+    print('sorted_hand_bids', sorted_hand_bids)
 
 
 def second():
     pass
 
 
-second()
+first()
--- 2023/8/ans.py
+++ 2023/8/ans.py
@@ -28,9 +28,35 @@ def first():
         i = (i+1) % n
     print(steps)
 
-
+# Try for individual ones
 def second():
-    pass
+    start_keys = []
+    for key in node_map:
+        if key[-1] == 'A':
+            start_keys.append(key)
+    count = len(start_keys)
+    n = len(directions)
+    i = 0
+    steps = 0
+    print(start_keys)
+
+    while True:
+        li = []
+        cur_count = 0
+        while len(start_keys):
+            key = start_keys.pop()
+            index = 0 if directions[i] == 'L' else 1
+            node = node_map[key][index]
+            li.append(node)
+            if node[-1] == 'Z':
+                cur_count += 1
+        start_keys = li
+        steps += 1
+        i = (i+1) % n
+        if cur_count == count:
+            break
+    print(steps)
 
 
-first()
+
+second()
