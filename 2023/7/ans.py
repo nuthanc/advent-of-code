3ec060941ac13493a4a5af8a965be08517e8dd02
nuthanc <rovanova.nuthan@gmail.com>
2023-12-18 11:42:25 +0530

    Update

--- 2023/7/ans.py
+++ 2023/7/ans.py
@@ -7,6 +7,14 @@ file_path = path.join(THIS_DIR, 'input.txt')
 with open(file_path) as f:
     lines = f.read().splitlines()
 
+card_strength = {
+    'A': 5,
+    'K': 4,
+    'Q': 3,
+    'J': 2,
+    'T': 1
+}
+
 hand_bids = []
 for line in lines:
     hand, bid_str = line.split()
@@ -18,28 +26,42 @@ def comparator(a, b):
     second_hand_counter = Counter(second_hand)
     first_hand_counter_values = sorted(first_hand_counter.values())
     second_hand_counter_values = sorted(second_hand_counter.values())
+    n = len(first_hand_counter_values)
+    m = len(first_hand_counter_values)
 
     if first_hand_counter_values[-1] == second_hand_counter_values[-1]:
         # First Counter values Comparison
-        ind = len(first_hand_counter_values) - 2
-        while ind >= 0 and first_hand_counter_values[ind] == second_hand_counter_values[ind]:
-            ind -= 1
-        if ind >= 0:
-            return first_hand_counter_values[ind] - second_hand_counter_values[ind]
+        index_first, index_second = n - 2, m - 2
+        while index_first >= 0 and index_second >= 0 and first_hand_counter_values[index_second] == second_hand_counter_values[index_second]:
+            index_first -= 1
+            index_second -= 1
+        if index_first >= 0 and index_second >= 0:
+            return first_hand_counter_values[index_first] - second_hand_counter_values[index_second]
+        elif index_first >= 0:
+            return -1
+        elif index_second >= 0:
+            return 1
         # Next Hand Lexicographic Comparison
         # Need to handle for 'A' > 'Q' and other character cases
-        if first_hand < second_hand:
-            return -1
-        else:
-            return 1
+        for i in range(len(first_hand)):
+            first_hand_ch, second_hand_ch = first_hand[i], second_hand[i]
+            if first_hand_ch == second_hand_ch:
+                continue
+            if first_hand_ch.isalpha() and second_hand_ch.isalpha():
+                return card_strength[first_hand_ch] - card_strength[second_hand_ch]
+            else:
+                return ord(first_hand_ch) - ord(second_hand_ch)
     else:
         return first_hand_counter_values[-1] - second_hand_counter_values[-1]
     
 
 def first():
     sorted_hand_bids = sorted(hand_bids, key=cmp_to_key(comparator))
-    print('sorted_hand_bids', sorted_hand_bids)
-
+    s = 0
+    for i in range(len(sorted_hand_bids)):
+        _, bid = sorted_hand_bids[i]
+        s += (i+1) * bid
+    print(s)
 
 def second():
     pass
