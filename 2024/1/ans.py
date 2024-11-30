a75281e94e6674533f3730083705add6b6abae3f
nuthanc <rovanova.nuthan@gmail.com>
2024-11-30 13:17:31 +0530

    Update

--- 2024/1/ans.py
+++ 2024/1/ans.py
@@ -1,8 +1,6 @@
-# https://adventofcode.com/2023/day/1
-# Missed this duplicate case: seven6seven, 6one6
+# https://adventofcode.com/2024/day/1
 
 from os import path
-from re import findall, search
 
 THIS_DIR = path.dirname(__file__)
 file_path = path.join(THIS_DIR, 'input.txt')
@@ -11,54 +9,12 @@ with open(file_path) as f:
 
 
 def first():
-    s = 0
-    for line in lines:
-        numbers = findall('\d', line)
-        if numbers:
-            s += int(numbers[0] + numbers[-1])
-    print(s)
+    pass
 
 
 def second():
-    digits = ['one', 'two', 'three', 'four',
-              'five', 'six', 'seven', 'eight', 'nine']
-    digits_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    s = 0
-    for line in lines:
-        numbers = findall('\d', line)
-        first_digit = None
-        first_digit_index = None
-        last_digit = None
-        last_digit_index = None
-        if numbers:
-            first_digit = numbers[0]
-            last_digit = numbers[-1]
-            first_digit_index = search(first_digit, line).start()
-            last_digit_index = line.rfind(last_digit)
-        for i in range(len(digits)):
-            digit = digits[i]
-            match = search(digit, line)
-            if match:
-                index = match.start()
-                if first_digit:
-                    if index < first_digit_index:
-                        first_digit = digits_num[i]
-                        first_digit_index = index
-                else:
-                    first_digit = digits_num[i]
-                    first_digit_index = index
-
-                index = line.rfind(digit)
-                if last_digit:
-                    if index > last_digit_index:
-                        last_digit = digits_num[i]
-                        last_digit_index = index
-                else:
-                    last_digit = digits_num[i]
-                    last_digit_index = index
-        s += int(first_digit + last_digit)
-    print(s)
+    pass
 
 
-# first()
-second()
+first()
+# second()
