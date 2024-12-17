2ce7040f96646afa43b3bcff35e21a985d87c329
nuthanc <rovanova.nuthan@gmail.com>
2024-12-17 07:54:13 +0530

    Correct dfs way

--- 2024/16/ans.py
+++ 2024/16/ans.py
@@ -1,4 +1,7 @@
 from os import path
+import sys
+
+sys.setrecursionlimit(1500)
 
 THIS_DIR = path.dirname(__file__)
 file_path = path.join(THIS_DIR, "input.txt")
@@ -24,8 +27,7 @@ rotate_directions = {
 
 def dfs(r, c, dr, dc):
     if lines[r][c] == "E":
-        print(cost_matrix[r][c])
-        return True
+        return
     new_row, new_col = r + dr, c + dc
     within_bounds = (
         new_row >= 0
@@ -39,8 +41,7 @@ def dfs(r, c, dr, dc):
         and cost_matrix[r][c] + 1 < cost_matrix[new_row][new_col]
     ):
         cost_matrix[new_row][new_col] = cost_matrix[r][c] + 1
-        if dfs(new_row, new_col, dr, dc):
-            return True
+        dfs(new_row, new_col, dr, dc)
 
     for di, dj in rotate_directions[(dr, dc)]:
         new_row, new_col = r + di, c + dj
@@ -56,9 +57,7 @@ def dfs(r, c, dr, dc):
             and cost_matrix[r][c] + 1001 < cost_matrix[new_row][new_col]
         ):
             cost_matrix[new_row][new_col] = cost_matrix[r][c] + 1001
-            if dfs(new_row, new_col, di, dj):
-                return True
-    return False
+            dfs(new_row, new_col, di, dj)
 
 
 def first():
@@ -68,6 +67,10 @@ def first():
                 cost_matrix[i][j] = 0
                 dfs(i, j, 0, 1)  # Starting from East/Right direction
                 break
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == "E":
+                print(cost_matrix[i][j])
 
 def second():
     pass
